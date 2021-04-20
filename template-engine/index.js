53ba5d62b14643d978bdd315149238a1bd5e7297
matiasbobek <matiasbobek@gmail.com>
2021-04-20 14:51:27 -0300

    Creates a SoccerTeam class and maps the API
    To send an array of soccer teams (json objects) to the handlebars layout

--- template-engine/index.js
+++ template-engine/index.js
@@ -2,31 +2,47 @@
 /* eslint-disable no-sequences */
 /* eslint-disable linebreak-style */
 
+class SoccerTeam {
+  constructor(name, crestUrl) {
+    this.name = name;
+    this.crestUrl = crestUrl;
+  }
+}
+
+function mapsTeams(teamsAPI) {
+  const teams = [];
+  teamsAPI.forEach((team) => {
+    teams.push(new SoccerTeam(team.name, team.crestUrl));
+  });
+  return (teams);
+}
+
 const express = require('express');
 const exphbs = require('express-handlebars');
+const fs = require('fs');
+const multer = require('multer');
 
 const port = 8080;
 
 const app = express();
 const handlebars = exphbs.create();
 
+const upload = multer({ dest: './uploads/team-logo' });
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.static(`${__dirname}/uploads`));
+
 app.get('/', (req, res) => {
+  const teams = JSON.parse(fs.readFileSync('../data/teams.json'));
+  console.log(mapsTeams(teams));
   res.render('teams', {
     layout: 'main',
     data: {
-      name: 'pepe',
+      teams: mapsTeams(teams),
     },
   });
-
-  //task: creates a object with the information of teams.json
- /* fetch('data/teams.json')
-    .then((teams) => teams.json())
-    .then((teamsJSON) => console.log(teamsJSON));
-*/
-
 });
 
 console.log(`i'm listening at http://localhost:${port}`);
